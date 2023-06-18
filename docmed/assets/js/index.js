c2b090f311d693111b3c9641aec9f7ea1482c501
FidanAsadova <7ksn2xn@code.edu.az>
2023-06-18 04:13:13 +0400

    feat: doctor cards on the homepage are dynamic

--- docmed/assets/js/index.js
+++ docmed/assets/js/index.js
@@ -1,18 +1,21 @@
 let BASE_URL_blog = "http://localhost:8080/blog";
+let BASE_URL_doctor = "http://localhost:8080/doctors";
 let blogs = document.querySelector(".card-blog");
+let doctors = document.querySelector(".card-doctor");
 
+///BLOG
 async function getAllBlogs() {
   let res = await axios.get(BASE_URL_blog);
   let data = res.data;
   console.log(data);
-  drawBlogs(data)
+  drawBlogs(data);
 }
 getAllBlogs();
 
 function drawBlogs(array) {
   blogs.innerHTML = "";
-  array.forEach(element => {
-    blogs.innerHTML +=`
+  array.forEach((element) => {
+    blogs.innerHTML += `
     <div class="col col-sm-12 col-md-6 col-lg-4 p-3 mb-3 mt-3">
               <div class="department-card">
                 <div class="img">
@@ -24,6 +27,35 @@ function drawBlogs(array) {
                 </div>
               </div>
             </div>
-    `
+    `;
+  });
+}
+
+////DOCTORS
+
+async function getAllDoctors() {
+  let res = await axios.get(BASE_URL_doctor);
+  let data = res.data;
+  console.log(data);
+  drawDoctors(data);
+}
+getAllDoctors();
+
+function drawDoctors(array) {
+  doctors.innerHTML = "";
+  array.forEach((element) => {
+    doctors.innerHTML += `
+    <div class="col col-sm-12 col-md-6 col-lg-3 mt-1 mb-1 pt-2 pb-2">
+              <div class="doctor">
+                <div class="img">
+                  <img class="w-100" src="${element.photo}" alt="doctor" />
+                </div>
+                <div class="doctor-about">
+                  <p class="doctor-name p-0 m-0">${element.firstName} ${element.lastName}</p>
+                  <p class="doctor-status p-0 m-0">${element.speciality}</p>
+                </div>
+              </div>
+            </div>
+    `;
   });
 }
