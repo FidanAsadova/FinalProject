83350a69505ea8e637595d1a46d3f560da048873
FidanAsadova <7ksn2xn@code.edu.az>
2023-06-19 23:06:55 +0400

    feat: admin panel-patients has been made dynamic

---
+++ docmed/admin-panel/assets/js/patient.js
@@ -0,0 +1,49 @@
+let BASE_URL_patient = "http://localhost:8080/patients";
+let patientTbody = document.querySelector(".patient-tbody");
+
+async function getPatientData() {
+  let res = await axios.get(BASE_URL_patient);
+  let data = res.data;
+  console.log(data);
+  drawPatient(data);
+}
+getPatientData();
+
+async function drawPatient(array) {
+  patientTbody.innerHTML = "";
+  array.forEach((patient) => {
+    patientTbody.innerHTML += `
+        <tr>
+                        <th scope="row"><img src="${patient.photo}" alt="..." /></th>
+                        <td>${patient.name}</td>
+                        <td>${patient.email}</td>
+                        <td>25-05-2023</td>
+                        <td>${patient.number}</td>
+                        <td>${patient.doctor}</td>
+                        <td>${patient.condition}</td>
+                        <td>
+                          <div class="btns d-flex justify-content-around">
+                            <a href=""
+                              ><i
+                                class="fa-regular fa-pen-to-square"
+                                style="color: navy"
+                              ></i
+                            ></a>
+                            <a onclick="deletePatient(${patient.id},this)" href=""
+                              ><i
+                                class="fa-regular fa-trash-can"
+                                style="color: darkred"
+                              ></i
+                            ></a>
+                          </div>
+                        </td>
+                      </tr>
+        `;
+  });
+}
+
+async function deletePatient(id, btn) {
+    await axios.delete(`${BASE_URL_patient}/${id}`);
+    btn.closest("tr").remove();
+  }
+  
